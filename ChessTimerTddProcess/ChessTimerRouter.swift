bb83d22b761ca7274d012533e00a6f6813c3958b
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-10-08 22:40:23 +0530

    setting up files for PlayerTimer

---
+++ ChessTimerTddProcess/ChessTimerRouter.swift
@@ -0,0 +1,6 @@
+
+import Foundation
+
+protocol RouterProtocol {
+    func routeToPlayerTimer()
+}
---
+++ ChessTimerTddProcess/ChessTimerRouterProtocol.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+class Router: RouterProtocol {
+    
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func routeToPlayerTimer() {
+
+    }
+}
